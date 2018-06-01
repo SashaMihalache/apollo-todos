33ab035e9c0d8cf581a83da95f1b6252b27c75cd
sasha <sasha.mihalache@gmail.com>
2018-06-01 22:36:59 +0300

    Add eslint and prettier for client and server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        Hello world
-      </div>
-    );
+    return <div>Hello world</div>;
   }
 }
 

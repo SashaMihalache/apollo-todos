9b42a1ac7a074f1a7ed993899d6199de829e72b1
sasha <sasha.mihalache@gmail.com>
2018-06-01 23:35:39 +0300

    Add apollo on client side and basic query

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
 import './App.css';
 
 class App extends Component {
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
 import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
+
+client
+  .query({
+    query: gql`
+      {
+        todos {
+          _id
+          title
+          isChecked
+        }
+      }
+    `,
+  })
+  .then(result => {
+    console.log(result);
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

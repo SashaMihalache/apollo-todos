51ab7d0e71175fc02f86fd0b339ba382fb319fbf
sasha <sasha.mihalache@gmail.com>
2018-06-02 12:18:16 +0300

    Prepare for todo mutation

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import './Todo.scss';
 
 class Todo extends React.Component {
--- client/src/views/todo/SingleTodoView.js
+++ client/src/views/todo/SingleTodoView.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { graphql } from 'react-apollo';
+import { GET_TODO } from '../../graphql/queries';
 import './SingleTodoView.scss';
 
 class SingleTodoView extends Component {
-  componentDidMount() {
-    console.log('ello', this.props.match);
-  }
+  handleSubmit = e => {
+    e.preventDefault();
+  };
 
   render() {
+    if (this.props.data.loading) {
+      return <p className="loading">Loading...</p>;
+    }
+    if (this.props.data.error) {
+      return <p className="error">Error...</p>;
+    }
+
+    const { title, isChecked } = this.props.data.todo;
+
     return (
-      <div>
+      <div className="single-todo-form">
         <h1>Single Todo</h1>
-        {this.props.match.params.todoId}
+
+        <form className="single-todo" onSubmit={this.handleSubmit}>
+          <div className="title-input">
+            Title:
+            <input type="text" defaultValue={title} />
+          </div>
+          <div className="is-checked-input">
+            Done:
+            <input type="checkbox" defaultChecked={isChecked} />
+          </div>
+
+          <button>Save Changes</button>
+        </form>
       </div>
     );
   }
 }
 
-export default SingleTodoView;
+export default graphql(GET_TODO, {
+  options: ownProps => ({
+    variables: {
+      id: ownProps.match.params.todoId,
+    },
+  }),
+})(SingleTodoView);
 
 SingleTodoView.propTypes = {
-  match: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired,
 };
